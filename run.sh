ec82614ac812a921ec2c6337ca3ab24c3d125ef9
Justin Kelly <justin@kelly.org.au>
2016-04-20 00:32:05 +1000

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@
 #/s3 --region "${AWS_REGION}" sync s3://docker-files.invoice.im/${DOMAIN}/logo/ /app/templates/invoices/logos/
 #/s3 --region "${AWS_REGION}" sync s3://docker-files.invoice.im/${DOMAIN}/template/ /app/templates/invoices/
 
-chmod -Rf 777 /srv/tmp
+chmod -Rf 755 /srv/tmp
 
 exec caddy --conf /etc/Caddyfile
