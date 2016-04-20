152e32ec641dc6bf03de182186d4c2cb176ddd79
Justin Kelly <justin@kelly.org.au>
2016-04-20 12:55:19 +1000

    ssmtp

--- run.sh
+++ run.sh
@@ -3,6 +3,10 @@
 #/s3 --region "${AWS_REGION}" sync s3://docker-files.invoice.im/${DOMAIN}/logo/ /app/templates/invoices/logos/
 #/s3 --region "${AWS_REGION}" sync s3://docker-files.invoice.im/${DOMAIN}/template/ /app/templates/invoices/
 
-#chown -Rf www-data:www-data /srv/tmp*
+
+echo "mailhub=${SMTP_HOST}:${SMTP_PORT}" > /etc/ssmtp/ssmtp.conf
+echo "AuthUser=${SMTP_USER}" >> /etc/ssmtp/ssmtp.conf
+echo "AuthPass=${SMTP_PASS}" >> /etc/ssmtp/ssmtp.conf
+echo "UseSTARTTLS=YES" >> /etc/ssmtp/ssmtp.conf
 
 exec caddy --conf /etc/Caddyfile
