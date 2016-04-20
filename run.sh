be17066df37274c0a3cb5d9c32a0cfde634fc43b
Justin Kelly <justin@kelly.org.au>
2016-04-20 10:39:22 +1000

    tmp perm

--- run.sh
+++ run.sh
@@ -3,6 +3,7 @@
 #/s3 --region "${AWS_REGION}" sync s3://docker-files.invoice.im/${DOMAIN}/logo/ /app/templates/invoices/logos/
 #/s3 --region "${AWS_REGION}" sync s3://docker-files.invoice.im/${DOMAIN}/template/ /app/templates/invoices/
 
-chmod -Rf 755 /srv/tmp
+chmod -Rf 775 /srv/tmp/log
+chmod -Rf 775 /srv/tmp/cache
 
 exec caddy --conf /etc/Caddyfile
